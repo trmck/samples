c89d7035a635caf1ae5b16135def201b56ef8e67
Tre`Von McKay <trmck@microsoft.com>
2016-10-21 15:55:47 -0700

    Inits Store context and fetches addons

--- IoTCustomStoreFront/IoTCustomStoreFront/MainPage.xaml.cs
+++ IoTCustomStoreFront/IoTCustomStoreFront/MainPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Services.Store;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +24,46 @@ namespace IoTCustomStoreFront
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private StoreContext _storeContext = null;
+
+
+        private void InitStoreContext()
+        {
+            _storeContext = StoreContext.GetDefault();
+        }
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            GetAddons();
+        }
+
+        public async void GetAddons()
+        {
+            if (_storeContext == null)
+                InitStoreContext();
+
+            // Specify the kinds of add-ons to retrieve.
+            string[] productKinds = { "Durable", "Consumable", "UnmanagedConsumable" };
+            List<String> filterList = new List<string>(productKinds);
+
+            StoreProductQueryResult queryResult = await _storeContext.GetAssociatedStoreProductsAsync(filterList);
+
+            if (queryResult.ExtendedError != null)
+            {
+                // The user may be offline or there might be some other server failure.
+                Debug.WriteLine(queryResult.ExtendedError.Message);
+                return;
+            }
+
+            foreach (KeyValuePair<string, StoreProduct> item in queryResult.Products)
+            {
+                // Access the Store product info for the add-on.
+                StoreProduct product = item.Value;
+                Debug.WriteLine(product);
+                // Use members of the product object to access listing info for the add-on...
+            }
         }
     }
 }
